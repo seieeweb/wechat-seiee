b4f076f4961e578a2f45ea5a3d4d94e083e0eae0
hebingchang <hebingchang1@live.com>
2018-02-17 01:22:46 +0800

    Add use Log

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -29,11 +29,7 @@ class WeChatController extends BaseController
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $app = app('wechat.official_account');
-        $app->server->push(function ($message) {
-            return json_encode($message);
-
-            // ...
-        });
+        $app->server->push('welcome');
 
         return $app->server->serve();
     }
