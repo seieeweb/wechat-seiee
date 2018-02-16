a29a1ddb168b5e070a5d0fc005453344f82b8b88
hebingchang <hebingchang1@live.com>
2018-02-17 00:30:21 +0800

    First init

---
+++ app/Http/Controllers/WeChatController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class WeChatController extends BaseController
+{
+    public function __construct()
+    {
+
+    }
+
+    public function serve()
+    {
+        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+
+        $app = app('wechat.official_account');
+        $app->server->push(function($message){
+            return "欢迎关注 overtrue！";
+        });
+
+        return $app->server->serve();
+    }
+}
